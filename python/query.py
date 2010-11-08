a77f6dc0d82dbadfcb08bec84d8bc455fa9dce2b
Sean Chen <sean.chen@leocorn.com>
2010-11-08 14:22:40 -0500

    change to offer a option -k / --key for Einztein API key also adding some help description: $ python query.py -h

--- python/query.py
+++ python/query.py
@@ -1,3 +1,4 @@
+#! /usr/bin/env python
 
 # query.py
 
@@ -6,24 +7,65 @@ __docformat__ = 'plaintext'
 
 import urllib
 import urllib2
+import optparse
 import simplejson as json
 
-query_pattern = 'http://www.einztein.com/api/v1/courses.json?query=%s&key=%s'
-query = urllib.quote('YOUR QUERY STRING HERE')
-key = 'YOUR EINZTEIN.COM API KEY HERE'
-query_url = query_pattern % (query, key)
-json_response = urllib2.urlopen(query_url)
-query_result = json.loads(json_response.read())
+def query(auth_key, query_string):
 
-index = 0
-for item in query_result:
-    index = index + 1
-    print "==========="
-    print "Course %d" % index
-    print "==========="
-    for each in item.iteritems():
-        print '%s: %s' % (each[0], each[1])
+    """
+    perform a simple query based for the given string.
+    """
 
-print "------------------------------------------------"
-print 'found %d courses in total' % index
+    query_pattern = 'http://www.einztein.com/api/v1/courses.json?query=%s&key=%s'
+    query = urllib.quote(query_string)
 
+    query_url = query_pattern % (query, auth_key)
+    json_response = urllib2.urlopen(query_url)
+    query_result = json.loads(json_response.read())
+
+    # return the search result in JSON format.
+    return query_result
+
+def format_json_result(json_result):
+
+    """
+    print out the json result nicely.
+    """
+
+    index = 0
+    for item in json_result:
+        index = index + 1
+        print "==========="
+        print "Course %d" % index
+        print "==========="
+        for each in item.iteritems():
+            print '%s: %s' % (each[0], each[1])
+    
+    print "------------------------------------------------"
+    print 'found %d courses in total' % index
+
+def main():
+
+    """
+    the main entry for this query script.
+    """
+
+    cli_parser = optparse.OptionParser()
+    cli_parser.add_option('-k', '--key', dest='auth_key', 
+                          help='Specify your Einztein API key')
+
+    # set the usage message.
+    cli_parser.set_usage('%prog [options] SEARCH_STRING')
+
+    # parse the arguments from command line.
+    options, args = cli_parser.parse_args()
+
+    if options.auth_key is None or args == []:
+        cli_parser.print_help()
+        return
+
+    ret = query(options.auth_key, args[0])
+    format_json_result(ret)
+
+if __name__ == "__main__":
+    main()
